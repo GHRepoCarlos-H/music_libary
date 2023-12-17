1b9cc6ac52cdd13346b7ead2d12a569cd5ba91e7
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-12-17 14:41:01 -0800

    applied css

---
+++ src/components/GalleryItem.js
@@ -0,0 +1,66 @@
+//folder components file GalleryItem
+import { useState } from 'react'
+
+
+function GalleryItem(props) {
+    let [view, setView] = useState(false)
+
+    const simpleView = () => {
+        const simpleStyle = {
+            'width': '30vw',
+            'height': '25vh',
+            'border': '9px inset purple',
+            'margin': '5px',
+            'background-color': 'black',
+            'color': 'white'
+        }
+        return (
+            <div style={simpleStyle}>
+                <h3>{props.item.trackName}</h3>
+                <img src={props.item.artworkUrl100} alt={props.item.trackName} />
+                <h4>{props.item.collectionName}</h4>
+                
+
+            </div>
+        )
+    }
+
+    
+    const detailView = () => {
+        const detailStyle = {
+          'width': '50vw',  // Updated to cover the entire width
+          'height': '20vh',
+          'margin': '2px',
+          'backgroundRepeat': 'no-repeat',
+          'backgroundSize': 'cover',
+          'backgroundColor': 'black',
+          'color': 'white',
+        };
+      
+        return (
+          <div style={detailStyle}>
+            <img
+              src={props.item.artworkUrl100}
+              alt={props.item.trackName}
+              style={{ width: '25%', height: '100%', objectFit: 'cover' }}
+            />
+            <h2>{props.item.trackName}</h2>
+            <h3>{props.item.collectionName}</h3>
+            <h4>{props.item.primaryGenreName}</h4>
+            <h4>{props.item.releaseDate}</h4>
+          </div>
+        );
+      };
+      
+
+    return (
+        <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
+        
+            {/* This simple ternary shows the simple view when 'view' is false! */}
+            {view ? detailView() : simpleView()}
+
+        </div>
+    )
+
+}
+export default GalleryItem
